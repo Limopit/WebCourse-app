b2192937dcce574ff3d9af497869123ffacce029
Ilya Bytski <bytskiymtc@gmail.com>
2025-03-03 11:37:40 +0300

    [FIX] Fixed metadata issues

--- Backend/CourseAppCourseService/CourseAppCourseService_WebAPI/Program.cs
+++ Backend/CourseAppCourseService/CourseAppCourseService_WebAPI/Program.cs
@@ -73,7 +73,8 @@ builder.Services.AddGrpcClient<UserServiceRpc.UserService.UserServiceClient>(opt
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer(options =>
     {
-        options.Authority = "http://localhost:5001";
+        options.Authority = "https://localhost:5001";
+        options.RequireHttpsMetadata = false;
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
